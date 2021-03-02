94e7e9e59b4f15c4f95c190274fdd5a28ed6b507
LiL Fi <46967506+LilFi@users.noreply.github.com>
2021-03-02 08:33:18 +0300

    Update Task 1.py

--- 21/Task 1.py
+++ 21/Task 1.py
@@ -1,4 +1,9 @@
 #TASK 1
+"""
+This code allows a class to pick their class president. It helps store the tutors name, candidate names, number of students per class and the number of votes. It will finally ouptut the name of the tutor, the votes of each candidate and the name of the candidate hat has won. In case of a tie in first place, then the program shall display who are the candidates that tied in first place.
+"""
+
+#Variables Used to store Data
 Candidate_Names = []
 Abstain = 0
 Candidate1 = 0
@@ -6,23 +11,25 @@ Candidate2 = 0
 Candidate3 = 0
 Candidate4 = 0
 
+#Beginning of the code, preparing
 Tutor_Name = input("Please enter the tutor's name: ")
 Number_Students = int(input("Please enter the number of students: "))
 Number_Candidates = int(input("Please enter the number of candidates, maximum 4: "))
 
-if Number_Candidates < 5:
-  for Candidate in range(Number_Candidates):
+#The body of the code
+if Number_Candidates < 5: #Start of and if statement
+  for Candidate in range(Number_Candidates): #Start of a for loop
     print("Please enter candidate", str(Candidate+1)+"'s name: ")
     Name = input()
-    Candidate_Names.append(Name)
+    Candidate_Names.append(Name) # End of the For loop
   
-  for Voting in range(Number_Students):
-    print ("\n")
-    print ("Enter 0 to Abstain")
-    for Instructions in range(Number_Candidates):
+  for Voting in range(Number_Students): #Starts of the second for loop
+    print ("\nEnter 0 to Abstain")
+    for Instructions in range(Number_Candidates): #Allows to output voting procedures which changes based on the number of candidates
       print ("Enter", Instructions+1, "to vote for", Candidate_Names[Instructions])
     Vote = int(input())
 
+    #If statements dedicated for voting
     if Vote == 0:
       Abstain = Abstain + 1
     elif Vote == 1:
@@ -32,11 +39,13 @@ if Number_Candidates < 5:
     elif Vote == 3:
       Candidate3 = Candidate3 + 1
     elif Vote == 4:
-      Candidate4 = Candidate4 + 1
+      Candidate4 = Candidate4 + 1 #End of the second for loop
     
   """
   I'm now havnig trouble printing cause it will be variable depending on the number of candidates
   """
+  #End of the if Statement
 
+#In case the tutor has entered more than 4 candidates
 else:
   print ("You have chosen more than 4 candidates.")
